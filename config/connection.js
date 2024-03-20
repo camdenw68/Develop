24df6582311f2f18bffe3219053ebf4e5116a04d
Camden Wierengo <CamdenPlaedu@gmail.com>
2024-03-20 18:30:06 -0400

    added something

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,21 @@
-// Assuming you are using Sequelize to connect to the database
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const { Sequelize } = require('sequelize');
+let sequelize;
 
-// Parse the DATABASE_URL to get connection information
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
-  protocol: 'mysql',
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false // For self-signed certificates
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306 
     }
-  }
-});
+  );
+}
 
-// Test the connection
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+module.exports = sequelize;
